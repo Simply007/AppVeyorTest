984fc45ac29745894e0cff61ef27e9288558d792
Ondřej Chrastina <chrastina.ondra@gmail.com>
2018-01-26 15:48:39 +0100

    initial commit

---
+++ AppVeyorTest/Capitalizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppVeyorTest
+{
+    public class Capitalizer
+    {
+        public string Capitalize(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
